752e3c3b56690af7da7adf372af701957c6a1bd1
jifchen07 <jifanche@usc.edu>
2021-09-25 22:24:56 -0700

    manual tuning

---
+++ src/PID.cpp
@@ -0,0 +1,43 @@
+#include "PID.h"
+#include <iostream>
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   */
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
+}
+
+void PID::UpdateError(double cte) {
+  /**
+   * TODO: Update PID errors based on cte.
+   */
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
+
+}
+
+double PID::TotalError() {
+  /**
+   * TODO: Calculate and return the total error
+   */
+  std::cout << "P: " << -Kp * p_error << std::endl;
+  std::cout << "D: " << -Kd * d_error << std::endl;
+  std::cout << "P + D = " << -Kp * p_error - Kd * d_error << std::endl;
+  return Kp * p_error + Ki * i_error + Kd * d_error;
+}
